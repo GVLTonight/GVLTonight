500b705730315c0b7f152ff5f773821229afbc41
Kevin Bost <kevbost@gmail.com>
2017-04-19 23:57:01 -0400

    converted smileys.js to Promises

--- src/main.js
+++ src/main.js
@@ -5,14 +5,34 @@ var clog = console.log;
 var _fbEvents = [];
 var _smileysEvents = [];
 
-fb_events.fb_data(function(fb_event_data){
-    _fbEvents.push(fb_event_data);
-    clog(fb_event_data.venues.length);
-    return _fbEvents;
-});
+// fb_events.fb_data(function(fb_event_data){
+//     _fbEvents.push(fb_event_data);
+//     clog(fb_event_data.venues.length);
+//     return _fbEvents;
+// });
 
-smileys.get_smileys_data(function(smileys_event_data){
-    _smileysEvents.push(smileys_event_data.items);
-    clog(smileys_event_data.items.length)
-    return _smileysEvents;
+// smileys.get_smileys_data(function(smileys_event_data){
+//     _smileysEvents.push(smileys_event_data.items);
+//     clog(smileys_event_data.items.length)
+//     return _smileysEvents;
+// });
+
+var _smileysData = function(){
+    return smileys() //.then(x => {console.log(x.items.length)});
+}
+
+var _facebookData = function(){
+    var idk = {"hi": "idk"};
+    return idk;
+}
+
+Promise.all([_facebookData(), _smileysData()]).then(arr => {
+    for (var i = 0; i < arr.length; i++) {
+        var element = arr[i];
+        if (i === 1){
+            for (var k = 0;k < element.items.length; k++) {
+                clog(arr[i].items[k].summary)
+            }
+        }
+    }
 });
--- src/smileys.js
+++ src/smileys.js
@@ -13,29 +13,31 @@ const request = require('request'),
       endDate = endDateBuffer.toISOString()
 ;
 
-exports.get_smileys_data = function(done){
-    getKey_and_init(function(result){
-        done(result);
-    })
-}
-
-var getKey_and_init = function(cb){
-    let url = 'https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23FFFFFF&src=smileysacousticcafe@gmail.com&color=%230D7813&ctz=America/New_York';
-    // get smileys api key for client6.google.com
-    request(url, function(error, response, html){
-        if(!error){
-            var key = response.body.match(/"developerKey":"(.+)"}/)[1];
-            getSmileysData(cb, key);
-        }
+function getSmileysKey () {
+    return new Promise((resolve, reject) => {
+        let url = 'https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23FFFFFF&src=smileysacousticcafe@gmail.com&color=%230D7813&ctz=America/New_York';
+        request(url, (err, res, body) => {
+            if (err) {
+                reject(err); return;
+            }
+            let key = res.body.match(/"developerKey":"(.+)"}/)[1];
+            resolve(key);
+        });
     });
 }
 
-var getSmileysData = function(done, key){
-    let url = 'https://clients6.google.com/calendar/v3/calendars/smileysacousticcafe@gmail.com/events?calendarId=smileysacousticcafe@gmail.com&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=' + startDate + '&timeMax=' + endDate + '&key=' + key;
-    request(url, function(error, response, html){
-        if(!error){
-            // console.log(response.body)
-            return done(JSON.parse(response.body));
-        }
+function getSmileysData () {
+    return new Promise((resolve, reject) => {
+        let key = getSmileysKey().then(function(key){
+            let url = 'https://clients6.google.com/calendar/v3/calendars/smileysacousticcafe@gmail.com/events?calendarId=smileysacousticcafe@gmail.com&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=' + startDate + '&timeMax=' + endDate + '&key=' + key;
+            request(url, function(err, res, html){
+                if (err) {
+                    reject(err); return;
+                }
+                resolve(JSON.parse(res.body));
+            });
+        });
     });
 }
+
+module.exports = getSmileysData;
