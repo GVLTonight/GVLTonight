2863e654d669c93d9ba81d0d8f2693c63b199fb7
Kevin Bost <kevbost@gmail.com>
2017-06-27 12:30:06 -0400

    merge testing.vue into gvl.vue primary route

--- src/components/Gvl.vue
+++ src/components/Gvl.vue
@@ -7,7 +7,7 @@
     </ul>
     <ul>
       <div class="venue">
-        <h1>gvltonight</h1>
+        <h1 v-on:click="sendGaEvent('page header', 'not-a-link', 'no-op')">gvltonight</h1>
         <hr class="title-divider">
         <ul>
           <li style="margin-top: 0">
@@ -17,96 +17,81 @@
         </ul>
       </div>
     </ul>
-    <ul v-if="collections && collections.length">
-      <div class="venue" v-for="col of collections" v-bind:key="col.temporaryId">
-        <h1><a v-bind:href="col.url">{{col.header}}</a></h1>
-        <hr class="title-divider">
-        <ul v-if="col.data.length > 0">
-          <li v-for="item of col.data" v-bind:key="item._id">
-            <div v-bind:class="{ today: item.isToday }">
-              <p v-if="item.sortBy === 'other'">
-                <a target="_blank" v-bind:href="item.venue.url">
-                  {{item.venue.name}}
-                </a>
-              </p>
-              <p>
-                <em>
-                  <a target="_blank" onClick="console.log(ga)" v-bind:href="item.url">
-                    {{item.title}}
-                  </a>
-                </em>
-              </p>
-              <p>
-                <span v-if="item.isToday">
-                  <span v-bind:class="{ green: item.isToday }">
-                    {{item.timeofday}}
-                  </span>:
-                </span>
-                {{item.date}}, {{item.time}}
-              </p>
-            </div>
-            <hr class="event-divider">
-          </li>
-        </ul>
-        <ul v-else>
-          <li>
-            <p><em>no listed upcoming events for this venue</em></p>
-            <hr class="event-divider">
-          </li>
-        </ul>
-      </div>
-    </ul>
+
+    <events-tonight :eventsTonight="tonightsEvents"></events-tonight>
+    <events-this-week :eventsThisWeek="thisWeeksEvents"></events-this-week>
+
   </div>
 </template>
 
 <script>
+import EventsTonight from '@/components/EventsTonight.vue'
+import EventsThisWeek from '@/components/EventsThisWeek.vue'
+import EventItem from '@/components/EventItem.vue'
 export default {
   data: () => ({
     errors: [],
-    collections: []
+    thisWeeksEvents: [],
+    tonightsEvents: []
   }),
 
-  created () {
-    axios.all([
-      axios.get(`https://greenvilletonight.com/api/thisweek`)
-    ])
-    // spread returns an array like spread(will_be_index[0], will_be_index[1])
-    .then(axios.spread((weekResponse) => {
-      // Converts array of objects into object keys based on sortBy value
-      return groupBy(weekResponse.data, 'sortBy')
-    }))
-    .then(rebuilt => {
-      // Adds useful values =>
-      // -- { collectionName: { header: 'string', url: 'string', sortOrder: Number, data: [ Array ] } }
-      for (let x in rebuilt) {
-        let _el = rebuilt[x]
-        let _venueName = _el[0].sortBy === 'other' ? _venueName = 'other' : _venueName = _el[0].venue.name
-        this.collections.push({
-          header: _venueName.toLowerCase(),
-          url: _el[0].venue.url,
-          sortOrder: _el[0].sortOrder,
-          temporaryId: Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36),
-          data: rebuilt[x].sort((a, b) => {
-            return new Date(a.datetime) - new Date(b.datetime)
-          })
-        })
-      }
-      return this.collections
-    })
-    .then(collectionsObject => {
-      return collectionsObject.forEach(topLevObj => {
-        topLevObj.data.map(innerObj => {
-          if (moment(innerObj.datetime).format(('YYYY-MM-DD')) === moment().format(('YYYY-MM-DD'))) {
-            innerObj.isToday = true
-            innerObj.timeofday = innerObj.datetime.split('T')[1] > '18:00:00' ? 'TONIGHT' : 'TODAY'
-          }
-          innerObj.date = moment(innerObj.datetime).format('ddd MM/DD/YYYY')
-          innerObj.time = moment(innerObj.datetime).format('h:mm A')
-        })
+  props: [
+    'eventsTonight',
+    'eventsThisWeek'
+  ],
+
+  components: {
+    EventsTonight,
+    EventsThisWeek,
+    EventItem
+  },
+
+  methods: {
+    sendGaEvent: function (category, url, title) {
+      this.$ga.event({
+        eventCategory: category,
+        eventAction: url,
+        eventLabel: title
       })
+    }
+  },
+
+  created () {
+    let tomorrow = moment().add(1, 'day').format('YYYY-MM-DD')
+    let endOfWeek = moment().add(7, 'day').format('YYYY-MM-DD')
+
+    axios.all([
+      // axios.get(`http://localhost:8000/api/tonight`),
+      // axios.get(`http://localhost:8000/api/events/${tomorrow}/${endOfWeek}`)
+      axios.get(`https://greenvilletonight.com/api/tonight`),
+      axios.get(`https://greenvilletonight.com/api/events/${tomorrow}/${endOfWeek}`)
+    ])
+    // axios.spread returns an array ie: function (will_be_index[0], will_be_index[1]) { }
+    .then(
+      axios.spread(function (tonight, week) {
+        return {
+          week: groupBy(week.data, 'sortBy'),
+          tonight: groupBy(tonight.data, 'sortBy')
+        }
+      })
+    )
+    .then(rebuilt => {
+      return {
+        week: rebuild(rebuilt, 'week'),
+        tonight: rebuild(rebuilt, 'tonight')
+      }
+    })
+    .then(thisWeeksEventsObject => {
+      this.thisWeeksEvents = thisWeeksEventsObject.week
+      this.tonightsEvents = thisWeeksEventsObject.tonight
+      accessorize(this.thisWeeksEvents)
+      accessorize(this.tonightsEvents)
     })
     .then(() => {
-      this.collections.sort(function (a, b) {
+      this.thisWeeksEvents.sort(function (a, b) {
+        return a.sortOrder - b.sortOrder
+      })
+      this.tonightsEvents.sort(function (a, b) {
         return a.sortOrder - b.sortOrder
       })
     })
@@ -126,59 +111,43 @@ function groupBy (arr, property) {
     return buffer
   }, {})
 }
+
+// Mutates values to be more human-readable friendly
+function accessorize (rebuiltObj) {
+  rebuiltObj.forEach(topLevObj => {
+    topLevObj.data.map(innerObj => {
+      if (moment(innerObj.datetime).format(('YYYY-MM-DD')) === moment().format(('YYYY-MM-DD'))) {
+        innerObj.isToday = true
+        innerObj.timeofday = innerObj.datetime.split('T')[1] > '18:00:00' ? 'TONIGHT' : 'TODAY'
+      }
+      innerObj.date = moment(innerObj.datetime).format('ddd MM/DD/YYYY')
+      innerObj.time = moment(innerObj.datetime).format('h:mm A')
+    })
+  })
+}
+
+// Adds useful values =>
+// -- { collectionName: { header: 'string', url: 'string', sortOrder: Number, data: [ Array ] } }
+function rebuild (rebuiltObj, param) {
+  let bufferArray = []
+  for (let x in rebuiltObj[param]) {
+    let _el = rebuiltObj[param][x]
+    let _venueName = _el[0].sortBy === 'other' ? _venueName = 'other' : _venueName = _el[0].venue.name
+    bufferArray.push({
+      header: _venueName.toLowerCase(),
+      url: _el[0].venue.url,
+      sortOrder: _el[0].sortOrder,
+      temporaryId: Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36),
+      data: rebuiltObj[param][x].sort((a, b) => {
+        return new Date(a.datetime) - new Date(b.datetime)
+      })
+    })
+  }
+  return bufferArray
+}
+
 </script>
 
 <style scoped>
-.hidden {
-  display: none;
-}
 
-h1 {
-  padding-top: 1.5em;
-  font-size: 2em;
-  font-weight: normal;
-}
-
-hr {
-  border-right: 200px solid #fff;
-  border-left: 200px solid #fff;
-}
-
-hr.title-divider {
-  border-top: 1px dashed #bbbbbb;
-}
-
-hr.event-divider {
-  width: 10px;
-  border-top: 1px dashed #eeeeee;
-}
-
-a {
-  color: inherit;
-  text-decoration: none;
-}
-
-a:hover {
-  background: red;
-  color: white;
-  font-style: normal;
-}
-
-
-ul {
-  list-style-type: none;
-}
-
-li>div {
-  padding: 0.4em 0;
-}
-
-.green {
-  color: #5b96b4;
-}
-
-.today {
-  color: #5bb477;
-  font-weight: bold;
-}
 </style>
